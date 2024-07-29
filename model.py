1622a527f98c9f714e7d38e0d3fc1451ac513676
toni-akintola <oakintol@nd.edu>
2024-07-29 11:53:05 -0400

    fixing boltzmann model to accomodate API

--- model.py
+++ model.py
@@ -4,22 +4,25 @@ import random
 from modelpy_abm.main import AgentModel
 
 
+def generateInitialData(model: AgentModel):
+    initial_data = {"wealth": 1}
+    return initial_data
+
+
+def generateTimestepData(model: AgentModel):
+    graph = model.get_graph()
+
+    for _node, node_data in graph.nodes(data=True):
+        if node_data["wealth"] > 0:
+            other_agent = random.choice(list(graph.nodes.keys()))
+            if other_agent is not None:
+                graph.nodes[other_agent]["wealth"] += 1
+                node_data["wealth"] -= 1
+
+    model.set_graph(graph)
+
+
 def constructModel() -> AgentModel:
-    def generateInitialData(model: AgentModel):
-        initial_data = {"wealth": 1}
-        return initial_data
-
-    def generateTimestepData(model: AgentModel):
-        graph = model.get_graph()
-
-        for _node, node_data in graph.nodes(data=True):
-            if node_data["wealth"] > 0:
-                other_agent = random.choice(list(graph.nodes.keys()))
-                if other_agent is not None:
-                    graph.nodes[other_agent]["wealth"] += 1
-                    node_data["wealth"] -= 1
-
-        model.set_graph(graph)
 
     model = AgentModel()
 
