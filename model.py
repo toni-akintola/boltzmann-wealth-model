2e2992f95d577f788258b141a5b84d509413eaa8
toni-akintola <oakintol@nd.edu>
2025-04-16 11:40:13 -0700

    updating boltzmann model

--- model.py
+++ model.py
@@ -1,8 +1,7 @@
 import networkx as nx
 import numpy as np
 import random
-from modelpy_abm.main import AgentModel
-import jsonpickle
+from emergent.main import AgentModel
 
 
 def generateInitialData(model: AgentModel):
@@ -28,31 +27,5 @@ def constructModel() -> AgentModel:
     model.set_initial_data_function(generateInitialData)
     model.set_timestep_function(generateTimestepData)
     model.update_parameters({"num_nodes": 3})
+    model["variations"] = ["base"]
     return model
-
-
-class ModelFunctions:
-    @staticmethod
-    def setGenerateInitialDataFunction(model: AgentModel):
-        initial_data = {"wealth": 1}
-        return initial_data
-
-    @staticmethod
-    def setTimestepDataFunction(model: AgentModel):
-        graph = model.get_graph()
-
-        for _node, node_data in graph.nodes(data=True):
-            if node_data["wealth"] > 0:
-                other_agent = random.choice(list(graph.nodes.keys()))
-                if other_agent is not None:
-                    graph.nodes[other_agent]["wealth"] += 1
-                    node_data["wealth"] -= 1
-        model.set_graph(graph)
-
-    @staticmethod
-    def constructModel() -> AgentModel:
-        model = AgentModel()
-        model.set_initial_data_function(ModelFunctions.generateInitialData)
-        model.set_timestep_function(ModelFunctions.generateTimestepData)
-        model.update_parameters({"num_nodes": 3})
-        return model
